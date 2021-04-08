2c4de8aa7fb4ad7c0e9e809806398575a21f3224
Chris Lee <simakov-art@mail.ru>
2021-04-09 00:24:45 +0700

    setting parse

--- analyze/parcing.py
+++ analyze/parcing.py
@@ -1,5 +1,10 @@
 import pandas as pd   # pip install pandas
 
-df = pd.read_excel('../stats/11/ЛИЦА.xls', sheet_name='ЛИЦА-1', index_col=0, comment='#')
+df = pd.read_excel('../stats/11/ЛИЦА.xls', sheet_name='ЛИЦА-1', index_col=0)
+df = df.reset_index()
+df = df.rename(columns=lambda x: x if not 'Unnamed' in str(x) else None)
+df = df.dropna(how='all')
+df.columns = pd.Series(df.columns).fillna(method='ffill', axis=0)
 print(df.to_string())
-print(df.columns.ravel())
+print(df.columns)
+
