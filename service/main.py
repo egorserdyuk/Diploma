15bf79b43d5333acdce5e4131bd0a69178016ac3
Egor <egor.serduck@gmail.com>
2021-06-28 10:25:14 +0700

    up

--- service/main.py
+++ service/main.py
@@ -1,197 +1,87 @@
-import os
-import pathlib
-import re
 import json
 import base64
 import datetime
 import io
-import dash_table
-from dash.exceptions import PreventUpdate
 
-from flask import send_from_directory
+import plotly.express as px
+import plotly.graph_objects as go
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 from dash.dependencies import Input, Output, State
-import cufflinks as cf
-
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(
     __name__,
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1.0"}
     ],
-    external_stylesheets=external_stylesheets,
 )
 
 app.title = "Интерактивная статистическая карта Алтайского края"
 server = app.server
 
-APP_PATH = str(pathlib.Path(__file__).parent.resolve())
-UPLOAD_DIRECTORY = "data/"
-UPLOADED_FILES = []
+DEFAULT_OPACITY = 0.8
 
-if not os.path.exists(UPLOAD_DIRECTORY):
-    os.makedirs(UPLOAD_DIRECTORY)
+mapbox_access_token = "pk.eyJ1IjoidGhlY29vbGR1bXAiLCJhIjoiY2txZjBieGl6MTlhNDJwbm1jeWd2bXJkeiJ9.gHVFvp1gZDOkVd6NZTkMow"
 
 world_path = 'data/' + 'map.geojson'
 with open(world_path) as f:
     geojson = json.load(f)
 
-default_df = pd.read_csv("data/test.csv", dtype={"County": str})
-
-YEARS = []
-dates = default_df.Date.values.tolist()
-YEARS += dates
-
-optionList = [{'label': i, 'value': i} for i in UPLOADED_FILES]
-optionList.insert(0, {'label': 'Subject', 'value': 'Data'})
-
-DEFAULT_OPACITY = 0.8
-
-mapbox_access_token = "pk.eyJ1IjoidGhlY29vbGR1bXAiLCJhIjoiY2txZjBieGl6MTlhNDJwbm1jeWd2bXJkeiJ9.gHVFvp1gZDOkVd6NZTkMow"
-mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
-
-app.layout = html.Div(
-    id="root",
-    children=[
-        html.Div(
-            id="header",
-            children=[
-                html.H4(children="Интерактивная статистическая карта Алтайского края"),
-                html.P(
-                    id="description",
-                    children="Данный ресурс поможет вам наглядно визуализировать любые имеющиеся статистические данные \
-                    Алтайского края по районам",
-                ),
-            ],
-        ),
-
-        html.Div(
-            id="app-container",
-            children=[
-                html.Div(
-                    id="left-column",
-                    children=[
-                        html.Div(
-                            id="slider-container",
-                            children=[
-                                html.P(
-                                    id="slider-text",
-                                    children="Выбор года:",
-                                ),
-                                dcc.Slider(
-                                    id="years-slider",
-                                    min=min(YEARS),
-                                    max=max(YEARS),
-                                    value=min(YEARS),
-                                    marks={
-                                        str(year): {
-                                            "label": str(year),
-                                            "style": {"color": "#7fafdf"},
-                                        }
-                                        for year in YEARS
-                                    },
-                                ),
-                            ],
-                        ),
-                        html.Div(
-                            id="heatmap-container",
-                            children=[
-                                html.P(
-                                    "Карта за {0} год".format(
-                                        min(YEARS)
-                                    ),
-                                    id="heatmap-title",
-                                ),
-                                dcc.Graph(
-                                    id="county-choropleth",
-                                    figure=dict(
-                                        layout=dict(
-                                            mapbox=dict(
-                                                layers=[],
-                                                accesstoken=mapbox_access_token,
-                                                style=mapbox_style,
-                                                center=dict(
-                                                    lat=52.78, lon=83.22
-                                                ),
-                                                pitch=0,
-                                                zoom=5,
-                                            ),
-                                            autosize=True,
-                                        ),
-                                    ),
-                                ),
-                            ],
-                        ),
-                    ],
-                ),
-                html.Div(
-                    id="graph-container",
-                    children=[
-                        html.P(id="chart-selector", children="Выберите диаграмму:"),
-                        dcc.Upload(
-                            id='upload-data',
-                            children=html.Div([
-                                'Drag and Drop or ',
-                                html.A('Select Files')
-                            ]),
-                            style={
-                                'width': '100%',
-                                'height': '60px',
-                                'lineHeight': '60px',
-                                'borderWidth': '1px',
-                                'borderStyle': 'dashed',
-                                'borderRadius': '5px',
-                                'textAlign': 'center',
-                                'margin': '10px'
-                            },
-                            # Allow multiple files to be uploaded
-                            multiple=True
-                        ),
-                        html.Div(id='output-data-upload'),
-                        dcc.Graph(
-                            id="selected-data",
-                            figure=dict(
-                                data=[dict(x=0, y=0)],
-                                layout=dict(
-                                    paper_bgcolor="#F4F4F8",
-                                    plot_bgcolor="#F4F4F8",
-                                    autofill=True,
-                                    margin=dict(t=75, r=50, b=100, l=50),
-                                ),
-                            ),
-                        ),
-                    ],
-                ),
-            ],
-        ),
-    ],
-)
+df = pd.read_csv("data/test.csv", dtype={"County": str})
+fig = go.Figure(px.choropleth_mapbox(df, geojson=geojson, color='Data',
+                                     locations='County', featureidkey="properties.County",
+                                     color_continuous_scale="Viridis",
+                                     range_color=(0, df.Data.max()),
+                                     center={"lat": 52.78, "lon": 83.22},
+                                     mapbox_style="carto-positron", opacity=0.5, zoom=5, animation_frame='Date'))
+fig.update_geos(fitbounds="locations", visible=False)
+fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
 
-def parse_contents(contents, filename, date):
+def read_file(contents, filename, date):
     content_type, content_string = contents.split(',')
 
     decoded = base64.b64decode(content_string)
+
     try:
         if 'csv' in filename:
             # Assume that the user uploaded a CSV file
             df = pd.read_csv(
                 io.StringIO(decoded.decode('utf-8')))
-
-            UPLOADED_FILES.append(filename)
-        elif 'xls' in filename:
+        elif 'xls' or 'xlsx' in filename:
             # Assume that the user uploaded an excel file
             df = pd.read_excel(io.BytesIO(decoded))
-            UPLOADED_FILES.append(filename)
     except Exception as e:
         print(e)
         return html.Div([
-            'Ошибка во время обработки файла.'
+            'There was an error processing this file.'
         ])
+
+    dates = df.Date.values.tolist()
+
+    if min(dates) < max(dates):
+        fig = go.Figure(px.choropleth_mapbox(df, geojson=geojson, color='Data',
+                                             locations='County', featureidkey="properties.County",
+                                             color_continuous_scale="Viridis",
+                                             range_color=(0, df.Data.max()),
+                                             center={"lat": 52.78, "lon": 83.22},
+                                             mapbox_style="carto-positron", opacity=0.5, zoom=5,
+                                             animation_frame='Date'))
+        fig.update_geos(fitbounds="locations", visible=False)
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    else:
+        fig = go.Figure(px.choropleth_mapbox(df, geojson=geojson, color='Data',
+                                             locations='County', featureidkey="properties.County",
+                                             color_continuous_scale="Viridis",
+                                             range_color=(0, df.Data.max()),
+                                             center={"lat": 52.78, "lon": 83.22},
+                                             mapbox_style="carto-positron", opacity=0.5, zoom=5))
+        fig.update_geos(fitbounds="locations", visible=False)
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     return html.Div([
         html.H5(filename),
@@ -213,6 +103,63 @@ def parse_contents(contents, filename, date):
     ])
 
 
+app.layout = html.Div(
+    id="root",
+    children=[
+        html.Div(
+            id="header",
+            children=[
+                html.H4(children="Интерактивная статистическая карта Алтайского края"),
+                html.P(
+                    id="description",
+                    children="Данный ресурс поможет вам наглядно визуализировать любые имеющиеся статистические данные \
+                    Алтайского края по районам",
+                ),
+            ],
+            style={
+                'textAlign': 'center',
+                'margin': '10px'
+            }
+        ),
+        html.Div(
+            id="app-container",
+            children=[
+                html.Div(
+                    id="heatmap-container",
+                    children=[
+                        dcc.Graph(
+                            figure=fig
+                        ),
+                    ],
+                ),
+            ],
+        ),
+        html.Div([
+            dcc.Upload(
+                id='upload-data',
+                children=html.Div([
+                    'Перетащите или ',
+                    html.A('Выберите файл')
+                ]),
+                style={
+                    'width': '100%',
+                    'height': '60px',
+                    'lineHeight': '60px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '5px',
+                    'textAlign': 'center',
+                    'margin': '10px'
+                },
+                # Allow multiple files to be uploaded
+                multiple=False
+            ),
+            html.Div(id='output-data-upload'),
+        ])
+    ],
+)
+
+
 @app.callback(Output('output-data-upload', 'children'),
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
@@ -220,78 +167,10 @@ def parse_contents(contents, filename, date):
 def update_output(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
         children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
+            read_file(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)
+        ]
         return children
-
-
-@app.callback(
-    Output("county-choropleth", "figure"),
-    [Input("years-slider", "value")],
-    [State("county-choropleth", "figure")],
-)
-def display_map(year, figure):
-    data = [
-        dict(
-            lat=default_df["County"],
-            type="scattermapbox",
-            hoverinfo="text",
-            marker=dict(size=5, color="white", opacity=0),
-        )
-    ]
-
-    annotations = [
-        dict(
-            showarrow=False,
-            align="right",
-            text="<b>Test1<br>Test2</b>",
-            font=dict(color="#2cfec1"),
-            bgcolor="#1f2630",
-            x=0.95,
-            y=0.95,
-        )
-    ]
-
-    if "layout" in figure:
-        lat = figure["layout"]["mapbox"]["center"]["lat"]
-        lon = figure["layout"]["mapbox"]["center"]["lon"]
-        zoom = figure["layout"]["mapbox"]["zoom"]
-    else:
-        lat = 38.72490
-        lon = -95.61446
-        zoom = 3.5
-
-    layout = dict(
-        mapbox=dict(
-            layers=[],
-            accesstoken=mapbox_access_token,
-            style=mapbox_style,
-            center=dict(lat=lat, lon=lon),
-            zoom=zoom,
-        ),
-        hovermode="closest",
-        margin=dict(r=0, l=0, t=0, b=0),
-        annotations=annotations,
-        dragmode="lasso",
-    )
-
-    geo_layer = dict(
-        sourcetype="geojson",
-        source=geojson,
-        type="fill",
-        color='Data',
-        opacity=0.5,
-        fill=dict(outlinecolor="#afafaf"),
-    )
-    layout["mapbox"]["layers"].append(geo_layer)
-
-    fig = dict(data=data, layout=layout)
-    return fig
-
-
-@app.callback(Output("heatmap-title", "children"), [Input("years-slider", "value")])
-def update_map_title(year):
-    return "Данные на карте за {0} год".format(year)
 
 
 if __name__ == '__main__':
